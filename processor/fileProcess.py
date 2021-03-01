686092dc7f885ff56d66cb5d09f1bba366967859
Sahil Hussain <sahil.hussain113@gmail.com>
2021-03-01 17:24:56 +0530

    reader/writer changes

--- processor/fileProcess.py
+++ processor/fileProcess.py
@@ -2,16 +2,28 @@ import json
 from pathlib import Path
 import re
 
-regex = re.compile(r'\>[a-zA-Z0-9_ !?.,]*\<')
+regex = re.compile(r'\>[a-zA-Z0-9_ !?:.,^\\n\\]*\<')
 
 def getKeydata():
     file = open('keyData.json')
     data = json.load(file)
     return data
 
+def newLineReader(text):
+    occcurences = re.findall(' [_ ]+ ',text)
+    for occurence in occcurences:
+        count = occurence.count('_')
+        text = text.replace(occurence,'\\n'*count)
+    return text
+
+def newLineWriter(text):
+    return text.replace('\\n',' _ ')
+
 def GetFileData(filePath):
     p = Path(filePath)
     text = p.read_text()
+    text = newLineWriter(text)
+
     localeData = regex.findall(text)
 
     for i in range(len(localeData)):
@@ -25,13 +37,14 @@ def saveFile(language,changedLocale,filePath,directoryPath):
 
     fileName = directoryPath+ '/'+language+'.xml'
 
-    print(fileName)
     changedFile = open(fileName,'w')
 
+    text = newLineWriter(text)
     localeData = regex.findall(text)
     updatedLocale = text
 
     for i in range(len(localeData)):
-        updatedLocale = re.sub(localeData[i],'>'+changedLocale[i]+'<',updatedLocale)
+        changedLocale[i] = newLineReader(changedLocale[i])
+        updatedLocale = updatedLocale.replace(localeData[i],'>'+changedLocale[i]+'<')
 
     changedFile.write(updatedLocale)
